ca1bddd7047db37321cd6de61c12863869478261
Yunfan REN <renyunfan@outlook.com>
2020-12-22 15:31:03 +0800

    first commit

--- main_controller/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yunfan/workspace/mav_ws/src/fly_with_t265/main_controller
-# Build directory: /home/yunfan/workspace/mav_ws/src/fly_with_t265/main_controller/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
