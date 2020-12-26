18c70bdffd2d48a2705325e1e51e2d00cb408f79
Yunfan REN <renyunfan@outlook.com>
2020-12-26 23:53:21 +0800

    Add show status

--- main_controller/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ main_controller/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -340,6 +340,7 @@ set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/actionlib_msgs_generate_messages_nodejs.dir/DependInfo.cmake"
   "CMakeFiles/mavros_msgs_generate_messages_eus.dir/DependInfo.cmake"
   "CMakeFiles/mavros_msgs_generate_messages_nodejs.dir/DependInfo.cmake"
+  "CMakeFiles/show_status.dir/DependInfo.cmake"
   "CMakeFiles/roscpp_generate_messages_nodejs.dir/DependInfo.cmake"
   "CMakeFiles/mavros_msgs_generate_messages_py.dir/DependInfo.cmake"
   "gtest/googlemock/CMakeFiles/gmock_main.dir/DependInfo.cmake"
